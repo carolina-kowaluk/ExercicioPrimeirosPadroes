1dc7c14bad159c835f5c2eee401543e259a1dcf3
pietra renner <pietra.renner@gmail.com>
2023-08-16 18:32:14 -0300

    adicionando DTO de data

---
+++ src/DataDTO.java
@@ -0,0 +1,31 @@
+import java.time.LocalDate;
+
+public class DataDTO {
+    private Integer dia;
+    private Integer mes;
+    private Integer ano;
+    
+    public Integer getDia(){
+        return dia;
+    }
+
+    public Integer getMes(){
+        return mes;
+    }
+
+    public Integer getAno(){
+        return ano;
+    }
+
+    public void setDia(){
+        this.dia = dia;
+    }
+
+    public void setMes(){
+        this.mes = mes;
+    }
+
+    public void setAno(){
+        this.ano = ano;
+    }
+}
